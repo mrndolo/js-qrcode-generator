8fb35a00c3d53cfb4481aa065dc2ba53d71d2e88
mrndolo <callistomutunga@gmail.com>
2024-06-11 08:41:12 +0300

    added form validation

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ let qrText = document.getElementById("qrText");
 // let pTag = document.getElementById("ptag");
 
 function generateQr(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-    console.log(qrImage.src);
+    if(qrText.value.length > 0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    // console.log(qrImage.src);
 
-    imgBox.classList.add("show-img");
-    console.log(imgBox.classList);
+        imgBox.classList.add("show-img");
+    // console.log(imgBox.classList);
+    }
+    else{
+        // pTag.classList.add("p-error");
+        qrText.classList.add("error");
+        qrText.style.border = `1px solid #ff0000`;
+        setTimeout(()=>{
+            qrText.classList.remove("error");
+            // pTag.classList.remove("p-error");
+            qrText.style.border = `1px solid #494eea`;
+        },1000)
+        // alert("Please enter something");
+    }
 }
